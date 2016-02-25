4a6711b22cc2b7694bcc873a72f3a3f106f1ef9c
Adam Luzsi <adamluzsi@gmail.com>
2016-02-25 23:38:09 +0100

    feat: add simple caching to template class

--- lib/rack/app/front_end/template.rb
+++ lib/rack/app/front_end/template.rb
@@ -3,6 +3,10 @@ class Rack::App::FrontEnd::Template
 
   require 'rack/app/front_end/template/default_layout'
 
+  def self.cache
+    @cache ||= Tilt::Cache.new
+  end
+
   def render(*args, &block)
     return render_result(*args, &block)
   end
@@ -17,15 +21,23 @@ class Rack::App::FrontEnd::Template
   def render_result(*args, &block)
     return Rack::App::File::Streamer.new(@file_path) unless it_is_a_template?
 
-    layout.render { Tilt.new(@file_path).render(*args, &block) }
+    layout.render { template.render(*args, &block) }
   end
 
   def it_is_a_template?
     not Tilt.templates_for(@file_path).empty?
   end
 
+  def template
+    get_template(@file_path)
+  end
+
   def layout
-    @options[:layout] ? Tilt.new(@options[:layout]) : DefaultLayout
+    @options[:layout] ? get_template(@options[:layout]) : DefaultLayout
+  end
+
+  def get_template(file_path)
+    self.class.cache.fetch(file_path) { Tilt.new(file_path) }
   end
 
 end
