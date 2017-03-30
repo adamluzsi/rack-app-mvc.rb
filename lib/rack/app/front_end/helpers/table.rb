f3e80d355e0b23fe5640401040c66641c5341c16
Adam Luzsi <adamluzsi@gmail.com>
2017-03-30 22:37:35 +0200

    feat: support tag attributes for table creator

--- lib/rack/app/front_end/helpers/table.rb
+++ lib/rack/app/front_end/helpers/table.rb
@@ -2,7 +2,7 @@ module Rack::App::FrontEnd::Helpers::Table
   module Builder
     extend(self)
 
-    def from_array_of_hash(array_of_hash)
+    def from_array_of_hash(array_of_hash, attributes)
       headers = array_of_hash.each_with_object([]) do |hash, trs|
         trs.push(*hash.keys)
         trs.uniq!
@@ -11,7 +11,7 @@ module Rack::App::FrontEnd::Helpers::Table
       o = Object.new
       o.extend(Rack::App::FrontEnd::Helpers::HtmlDsl)
 
-      table_html = o.__send__ :table_tag do
+      table_html = o.__send__ :table_tag, attributes do
         tr_tag do
           headers.each do |header|
             td_tag String(header)
@@ -30,11 +30,11 @@ module Rack::App::FrontEnd::Helpers::Table
       table_html
     end
 
-    def from_hash(hash)
+    def from_hash(hash, attributes)
       o = Object.new
       o.extend(Rack::App::FrontEnd::Helpers::HtmlDsl)
 
-      table_html = o.__send__ :table_tag do
+      table_html = o.__send__ :table_tag, attributes do
         hash.each do |key, value|
           tr_tag do
             td_tag String(key)
@@ -47,11 +47,11 @@ module Rack::App::FrontEnd::Helpers::Table
     end
   end
 
-  def table_by(object)
+  def table_by(object, attributes={})
     if object.is_a?(Array) && object.all? { |e| e.is_a?(Hash) }
-      Builder.from_array_of_hash(object)
+      Builder.from_array_of_hash(object, attributes)
     elsif object.is_a?(Hash)
-      Builder.from_hash(object)
+      Builder.from_hash(object, attributes)
     else
       raise("don't know how to build table from this: #{object}")
     end
--- spec/rack/app/front_end/helpers/table_spec.rb
+++ spec/rack/app/front_end/helpers/table_spec.rb
@@ -9,8 +9,10 @@ describe Rack::App::FrontEnd::Helpers::Table do
     o
   end
 
+  let(:attributes) { {} }
+
   describe '#table_by' do
-    subject { instance.table_by(object) }
+    subject { instance.table_by(object, attributes) }
 
     context 'when object is an array of hash' do
       let(:object) { [{ hello: 'world' }, { hello: 'programming', col: 'val' }] }
@@ -39,6 +41,35 @@ describe Rack::App::FrontEnd::Helpers::Table do
 
       HTML
       end
+
+      context 'and attributes given' do
+        let(:attributes) { { class: 'table' } }
+
+        it 'should create a html table based on the array of has content' do
+        is_expected.to eq uglier_html <<-HTML
+
+            <table class="table">
+
+              <tr>
+                <td>hello</td>
+                <td>col</td>
+              </tr>
+
+              <tr>
+                <td>world</td>
+                <td/>
+              </tr>
+
+              <tr>
+                <td>programming</td>
+                <td>val</td>
+              </tr>
+
+            </table>
+
+      HTML
+      end
+      end
     end
 
     context 'when object is a hash' do
@@ -62,6 +93,30 @@ describe Rack::App::FrontEnd::Helpers::Table do
             </table>
 
       HTML
+      end
+
+      context 'and attributes given' do
+        let(:attributes) { { class: 'table' } }
+
+        it 'should create a html table based on the hash content' do
+          is_expected.to eq uglier_html <<-HTML
+
+            <table class="table">
+
+              <tr>
+                <td>name</td>
+                <td>cat</td>
+              </tr>
+
+              <tr>
+                <td>lives</td>
+                <td>9</td>
+              </tr>
+
+            </table>
+
+      HTML
+        end
       end
     end
   end
